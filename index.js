1ab8ef5217202f915321eab60c80091eecb4ef35
Markus Dittmann <dmarku@posteo.de>
2020-03-12 16:43:35 +0100

    let prettier have its way with formatting

--- index.js
+++ index.js
@@ -34,24 +34,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const render = util.promisify(carbone.render);
 
 // Flagging default formatters to remove custom ones later
-_.forEach(carbone.formatters, formatter => formatter.$isDefault = true);
+_.forEach(carbone.formatters, formatter => (formatter.$isDefault = true));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(`./test.html`));
 });
 
-app.post('/render', upload.single(`template`), async (req, res) => {
+app.post("/render", upload.single(`template`), async (req, res) => {
   const template = req.file;
-  const originalNameWOExt = template.originalname.split(`.`).slice(0, -1).join(`.`);
+  const originalNameWOExt = template.originalname
+    .split(`.`)
+    .slice(0, -1)
+    .join(`.`);
   const originalFormat = template.originalname.split(`.`).reverse()[0];
   let data = req.body.data;
   let options = {};
   let formatters = {};
+
   try {
     options = JSON.parse(req.body.options);
   } catch (e) {}
+
   options.convertTo = options.convertTo || originalFormat;
-  options.outputName = options.outputName || `${originalNameWOExt}.${options.convertTo}`;
+  options.outputName =
+    options.outputName || `${originalNameWOExt}.${options.convertTo}`;
   if (typeof data !== `object` || data === null) {
     try {
       data = JSON.parse(req.body.data);
@@ -59,12 +65,16 @@ app.post('/render', upload.single(`template`), async (req, res) => {
       data = {};
     }
   }
+
   try {
     formatters = telejson.parse(req.body.formatters);
   } catch (e) {}
 
   // Removing previous custom formatters before adding new ones
-  carbone.formatters = _.filter(carbone.formatters, formatter => formatter.$isDefault === true);
+  carbone.formatters = _.filter(
+    carbone.formatters,
+    formatter => formatter.$isDefault === true
+  );
 
   carbone.addFormatters(formatters);
 
@@ -79,7 +89,11 @@ app.post('/render', upload.single(`template`), async (req, res) => {
 
   fs.remove(template.path);
 
-  res.setHeader(`Content-Disposition`, `attachment; filename=${options.outputName}`);
+
+  res.setHeader(
+    `Content-Disposition`,
+    `attachment; filename=${options.outputName}`
+  );
   res.setHeader(`Content-Transfer-Encoding`, `binary`);
 	res.setHeader(`Content-Type`, `application/octet-stream`);
   res.setHeader(`Carbone-Report-Name`, options.outputName);
@@ -87,4 +101,6 @@ app.post('/render', upload.single(`template`), async (req, res) => {
   return res.send(report);
 });
 
-app.listen(port, () => console.log(`Carbone wrapper listenning on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Carbone wrapper listenning on port ${port}!`)
+);
